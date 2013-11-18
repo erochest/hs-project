1296c958e3cf846a7dde71f0cb327ebb58b2f5f9
Eric Rochester <erochest@virginia.edu>
2013-11-18 11:15:54 -0500

    Updated test-suite section to use tasty.

--- specs/Specs.hs
+++ specs/Specs.hs
@@ -1,2 +1,21 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+
+module Main where
+
+
+import Test.Tasty
+-- import Test.Tasty.QuickCheck as QC
+-- import Test.Tasty.HUnit as HU
+import Test.Tasty.Hspec
+
+
+specs :: Spec
+specs = undefined
+
+tests :: TestTree
+tests = testGroup "main"
+    [ testCase "something" specs
+    ]
+
+main = defaultMain tests
 
